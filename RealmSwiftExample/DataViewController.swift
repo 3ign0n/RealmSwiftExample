65c4fed4b13c619969d94387829e84dd6ebbb55c
TAKEDA hiroyuki(aka 3ign0n) <take6a.hir0yuki@gmail.com>
2015-04-04 12:16:39 +0900

    add project files

---
+++ RealmSwiftExample/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  RealmSwiftExample
+//
+//  Created by 3ign0n on 2015/04/04.
+//  Copyright (c) 2015年 3ign0n. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
